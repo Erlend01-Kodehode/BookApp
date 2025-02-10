15a7e6cf46f3e9b831ff28298b361b64e8304bb7
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2025-02-10 08:54:55 +0100

    Create api.js

---
+++ src/Data/api.js
@@ -0,0 +1,32 @@
+const apiGeneral = "gutendex.com/books";
+
+const apiTopicFiction = `${apiGeneral}?topic=fiction`;
+const apiTopicMystery = `${apiGeneral}?topic=mystery`;
+const apiTopicThriller = `${apiGeneral}?topic=thriller`;
+const apiTopicRomance = `${apiGeneral}?topic=romance`;
+const apiTopicFantasy = `${apiGeneral}?topic=fantasy`;
+const apiTopicMorality = `${apiGeneral}?topic=morality`;
+const apiTopicSociety = `${apiGeneral}?topic=society`;
+const apiTopicPower = `${apiGeneral}?topic=power`;
+const apiTopicJustice = `${apiGeneral}?topic=justice`;
+const apiTopicAdventure = `${apiGeneral}?topic=adventure`;
+const apiTopicTragedy = `${apiGeneral}?topic=tragedy`;
+const apiTopicWar = `${apiGeneral}?topic=war`;
+const apiTopicPhilosophy = `${apiGeneral}?topic=philosophy`;
+
+export {
+  apiGeneral,
+  apiTopicFiction,
+  apiTopicMystery,
+  apiTopicThriller,
+  apiTopicRomance,
+  apiTopicFantasy,
+  apiTopicMorality,
+  apiTopicSociety,
+  apiTopicPower,
+  apiTopicJustice,
+  apiTopicAdventure,
+  apiTopicTragedy,
+  apiTopicWar,
+  apiTopicPhilosophy,
+};
